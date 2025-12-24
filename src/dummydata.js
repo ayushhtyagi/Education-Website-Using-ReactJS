dd0aa2a874f525329d1ba6c75c0c432fe5a66fba
ayushhtyagi <152645760+ayushhtyagi@users.noreply.github.com>
2025-12-24 15:03:31 +0530

    Add files via upload

---
+++ src/dummydata.js
@@ -0,0 +1,424 @@
+export const homeAbout = [
+    {
+      id: 1,
+      cover: "https://img.icons8.com/dotty/80/000000/storytelling.png",
+      title: "Online Courses",
+      desc: "Online learning offers flexibility, diverse resources, personalized pace, accessibility, skill development, cost efficiency, global networking, and convenience.",
+    },
+    {
+      id: 1,
+      cover: "https://img.icons8.com/ios/80/000000/diploma.png",
+      title: "Earn A Certificates",
+      desc: "Earning certificates through online learning platforms helps enhance skills, gain credibility, and open up new career opportunities with flexible, affordable access.",
+    },
+    {
+      id: 1,
+      cover: "https://img.icons8.com/ios/80/000000/athlete.png",
+      title: "Learn with Expert",
+      desc: "Learn from experts with personalized guidance, diverse insights, and access to cutting-edge knowledge anytime, anywhere.",
+    },
+  ]
+  export const awrapper = [
+    {
+      cover: "https://img.icons8.com/external-yogi-aprelliyanto-basic-outline-yogi-aprelliyanto/80/ffffff/external-graduation-education-yogi-aprelliyanto-basic-outline-yogi-aprelliyanto.png",
+      data: "3,000",
+      title: "SUCCESS STORIES",
+    },
+  
+    {
+      cover: "https://img.icons8.com/ios/80/ffffff/athlete.png",
+      data: "320",
+      title: "TRUSTED TUTORS",
+    },
+    {
+      cover: "https://img.icons8.com/external-outline-icons-maxicons/80/ffffff/external-calender-insurance-outline-outline-icons-maxicons.png",
+      data: "1,000",
+      title: "SCHEDULES",
+    },
+    {
+      cover: "https://img.icons8.com/ios/80/ffffff/macbook-idea--v3.png",
+      data: "587",
+      title: "COURSES",
+    },
+  ]
+  export const coursesCard = [
+    {
+      id: 1,
+      cover: "../images/courses/c1.png",
+      coursesName: "Introducing to Software Engineering",
+      courTeacher: [
+        {
+          dcover: "./images/back.webp",
+          name: "by Poonam Baghel",
+          totalTime: "50 lectures (190 hrs)",
+        },
+      ],
+      priceAll: "₹30000 All Course",
+      pricePer: "₹2500 per month",
+    },
+    {
+      id: 2,
+      cover: "../images/courses/c2.png",
+      coursesName: "Enhancing Adobe Photoshop Skills",
+      courTeacher: [
+        {
+          dcover: "./images/back.webp",
+          name: "by Ravi Prakash Singh",
+          totalTime: "30 lectures (125 hrs)",
+        },
+      ],
+      priceAll: "₹8000 All Course",
+      pricePer: "₹1000 per month",
+    },
+    {
+      id: 3,
+      cover: "../images/courses/c3.png",
+      coursesName: "Web Developers with API",
+      courTeacher: [
+        {
+          dcover: "./images/back.webp",
+          name: "by Megha Jakha",
+          totalTime: "50 lectures (50 hrs)",
+        },
+      ],
+      priceAll: "₹25000 All Course",
+      pricePer: "₹2000 per month",
+    },
+    {
+      id: 4,
+      cover: "../images/courses/c4.png",
+      coursesName: "Introducing to Programming with WordPress",
+      courTeacher: [
+        {
+          dcover: "./images/back.webp",
+          name: "by Prabin Thapa Mager",
+          totalTime: "20 lectures (20 hrs)",
+        },
+      ],
+      priceAll: "₹15000 All Course",
+      pricePer: "₹1600 per month",
+    },
+    {
+      id: 5,
+      cover: "../images/courses/c5.png",
+      coursesName: "Introducing to Programming with ReactJS",
+      courTeacher: [
+        {
+          dcover: "./images/back.webp",
+          name: "by Sunil Bishowkaram",
+          totalTime: "100 lectures (150 hrs)",
+        },
+      ],
+      priceAll: "₹40000 All Course",
+      pricePer: "₹3000 per month",
+    },
+    {
+      id: 6,
+      cover: "../images/courses/c6.png",
+      coursesName: "Introducing a Course for FullStack Developer",
+      courTeacher: [
+        {
+          dcover: "./images/back.webp",
+          name: "by Mary Bendicta Morais",
+          totalTime: "200 lectures (300 hrs)",
+        },
+      ],
+      priceAll: "₹25000 All Course",
+      pricePer: "₹2800 per month",
+    },
+    {
+      id: 7,
+      cover: "../images/courses/c7.png",
+      coursesName: "Introducing to with HTML / CSS",
+      courTeacher: [
+        {
+          dcover: "./images/back.webp",
+          name: "by Megha Jakhar",
+          totalTime: "20 lectures (50 hrs)",
+        },
+      ],
+      priceAll: "₹10000 All Course",
+      pricePer: "₹1000 per month",
+    },
+    {
+      id: 8,
+      cover: "../images/courses/c8.png",
+      coursesName: "Introducing to with JAVA",
+      courTeacher: [
+        {
+          dcover: "./images/back.webp",
+          name: "by Petter",
+          totalTime: "80 lectures (200 hrs)",
+        },
+      ],
+      priceAll: "₹20000 All Course",
+      pricePer: "₹1500 per month",
+    },
+    {
+      id: 9,
+      cover: "../images/courses/c9.png",
+      coursesName: "Introducing FrontEnd Developer",
+      courTeacher: [
+        {
+          dcover: "./images/back.webp",
+          name: "by Martha Jecientha ",
+          totalTime: "50 lectures (50 hrs)",
+        },
+      ],
+      priceAll: "₹35000 All Course",
+      pricePer: "₹3000 per month",
+    },
+  ]
+  export const online = [
+    {
+      cover: "./images/courses/online/o1.png",
+      hoverCover: "./images/courses/online/o1.1.png",
+      courseName: "UI/UX Design",
+      course: "25 Courses",
+    },
+    {
+      cover: "./images/courses/online/o2.png",
+      hoverCover: "./images/courses/online/o2.1.png",
+      courseName: "Art & Design",
+      course: "25 Courses",
+    },
+    {
+      cover: "./images/courses/online/o3.png",
+      hoverCover: "./images/courses/online/o3.1.png",
+      courseName: "Computer Science",
+      course: "10 Courses",
+    },
+    {
+      cover: "./images/courses/online/o4.png",
+      hoverCover: "./images/courses/online/o4.1.png",
+      courseName: "Archeologic",
+      course: "15 Courses",
+    },
+    {
+      cover: "./images/courses/online/o5.png",
+      hoverCover: "./images/courses/online/o5.1.png",
+      courseName: "Software ",
+      course: "40 Courses",
+    },
+    {
+      cover: "./images/courses/online/o6.png",
+      hoverCover: "./images/courses/online/o6.1.png",
+      courseName: "Electronics",
+      course: "30 Courses",
+    },
+    {
+      cover: "./images/courses/online/o7.png",
+      hoverCover: "./images/courses/online/o7.1.png",
+      courseName: "Health & Fitness",
+      course: "10 Courses",
+    },
+    {
+      cover: "./images/courses/online/o8.png",
+      hoverCover: "./images/courses/online/o8.1.png",
+      courseName: "Marketing",
+      course: "30 Courses",
+    },
+    {
+      cover: "./images/courses/online/o9.png",
+      hoverCover: "./images/courses/online/o9.1.png",
+      courseName: "Graphic Design",
+      course: "20 Courses",
+    },
+    {
+      cover: "./images/courses/online/o10.png",
+      hoverCover: "./images/courses/online/o10.1.png",
+      courseName: "Music",
+      course: "50 Courses",
+    },
+    {
+      cover: "./images/courses/online/o11.png",
+      hoverCover: "./images/courses/online/o11.1.png",
+      courseName: "Business",
+      course: "17 Courses",
+    },
+    {
+      cover: "./images/courses/online/o12.png",
+      hoverCover: "./images/courses/online/o12.1.png",
+      courseName: "Web Management",
+      course: "17 Courses",
+    },
+  ]
+  export const team = [
+    {
+      cover: "./images/team/t1.webp",
+      name: "Alice Brown",
+      work: "DEVELOPER AND LEAD INSTRUCTOR",
+      education: "Ph.D in Computer Science"
+    },
+    {
+      cover: "./images/team/t2.webp",
+      name: "Bob Wilson",
+      work: "DEVELOPER AND INSTRUCTOR",
+      education: "Ph.D in Software Engineering"
+    },
+    {
+      cover: "./images/team/t3.webp",
+      name: "Charlie Johnson",
+      work: "DEVELOPER AND INSTRUCTOR",
+      education: "Ph.D in Web Development"
+    },
+    {
+      cover: "./images/team/t4.webp",
+      name: "Diana Smith",
+      work: "DEVELOPER AND INSTRUCTOR",
+      education: "Ph.D in Graphic Design"
+    },
+    {
+      cover: "./images/team/t5.webp",
+      name: "Ethan Clark",
+      work: "DEVELOPER AND INSTRUCTOR",
+      education: "Ph.D in Information Technology"
+    },
+    {
+      cover: "./images/team/t6.webp",
+      name: "Fiona Lee",
+      work: "DEVELOPER AND INSTRUCTOR",
+      education: "Ph.D in Multimedia"
+    },
+    {
+      cover: "./images/team/t7.webp",
+      name: "George Martinez",
+      work: "DEVELOPER AND INSTRUCTOR",
+      education: "Ph.D in Computer Engineering"
+    },
+    {
+      cover: "./images/team/t8.webp",
+      name: "Hannah Roberts",
+      work: "DEVELOPER AND INSTRUCTOR",
+      education: "Ph.D in Digital Marketing"
+    }
+]
+
+  export const price = [
+    {
+      name: "BASIC PLAN",
+      price: "29K/year",
+      desc: "Beginner-friendly access to basic courses and community forums.",
+    },
+    {
+      name: "BEGINNER PLAN",
+      price: "39K/year",
+      desc: "Gaining foundational skills with live classes and personalized feedback.",
+    },
+    {
+      name: "PREMIUM PLAN",
+      price: "49K/year",
+      desc: "In-depth learning with advanced projects and one-on-one mentoring.",
+    },
+    {
+      name: "ULTIMATE PLAN",
+      price: "59K/year",
+      desc: "Professionals looking for the ultimate learning experience with career coaching.",
+    },
+  ]
+  export const faq = [
+    {
+      title: "How to Enroll This Online Courses?",
+      desc: "To enroll in online courses, simply visit the Academia platform and browse the available courses. Choose the course you’re interested in and click on “Enroll Now.” Complete the registration process to gain access to the course materials and start learning!",
+    },
+    {
+      title: "How do I access my course materials?",
+      desc:"After enrolling, you can log in to the Academia platform, navigate to your dashboard, and find the course listed under “My Courses.” Click on the course title to access the materials, including lectures, assignments, and resources."
+    },
+    {
+      title: "What if I miss a live class?",
+      desc:"No worries! All live classes are recorded and available in the course materials. You can watch them at your convenience, ensuring you don’t miss any part of the learning experience."
+    },
+    {
+      title: "How do I get support if I have questions about the course?",
+      desc:"You can reach out to the support team via the platform’s messaging system, or post your questions in the course discussion forum. Expert instructors and the community are always available to help!"  
+  },
+    {
+      title: "Are there any prerequisites for the courses?",
+      desc:"Each course may have recommended prerequisites listed on its page. Some courses are beginner-friendly, while others may require foundational knowledge in specific topics. Check the course details for specific prerequisites before enrolling."
+    },
+    {
+      title: " What if I need to pause or extend my course access?",
+      desc:"You can contact customer support to request an extension or a temporary pause in your course access. The team will assist you based on your situation."
+    },
+  ]
+  export const blog = [
+    {
+      id: 1,
+      type: "admin",
+      date: "JAN. 18, 2024",
+      com: "25 COMMENTS ",
+      title: "Discover the Best Learning Experience with Academia",
+      desc: "Academia has been a game-changer for me. The courses are well-structured, and the expert instructors make learning so enjoyable!",
+      cover: "../images/blog/b1.webp",
+    },
+    {
+      id: 2,
+      type: "admin",
+      date: "API. 25, 2024",
+      com: "5 COMMENTS ",
+      title: "Explore the fundamentals of software engineering and new technologies for your development.",
+      desc: "This course provided a strong foundation in software engineering concepts. Perfect for anyone looking to start their tech journey!",
+      cover: "../images/blog/b2.webp",
+    },
+    {
+      id: 3,
+      type: "user",
+      date: "MAY. 15, 2024",
+      com: "10 COMMENTS ",
+      title: "I Took the HTML, CSS, and JavaScript Course and Here’s What I Learned",
+      desc: "Web development always felt overwhelming, but this course made it simple and exciting. I can now build responsive websites confidently!",
+      cover: "../images/blog/b3.webp",
+    },
+    {
+      id: 4,
+      type: "user",
+      date: "FEB. 02, 2024",
+      com: "15 COMMENTS ",
+      title: "My Journey Through the WordPress Programming Course",
+      desc: "Building websites with WordPress was so easy with this course. The practical examples made learning fun and effective!",
+      cover: "../images/blog/b4.webp",
+    },
+    {
+      id: 5,
+      type: "admin",
+      date: "NOV. 14, 2024",
+      com: "10 COMMENTS ",
+      title: "Introducing to Programming with ReactJS",
+      desc: "ReactJS seemed complex at first, but this course broke it down beautifully. I now feel confident building modern web apps!",
+      cover: "../images/blog/b5.webp",
+    },
+    {
+      id: 6,
+      type: "user",
+      date: "JUL. 18, 2024",
+      com: "12 COMMENTS ",
+      title: "My Learning Journey with Academia",
+      desc: "I joined Academia to enhance my skills, and it’s been an amazing experience! The expert guidance and flexible courses boosted my confidence.",
+      cover: "../images/blog/b6.webp",
+    },
+  ]
+  export const testimonal = [
+    {
+      id: 1,
+      name: "John Doe",
+      post: " ReactJS Developer",
+      desc: "The ReactJS course was incredibly insightful! The hands-on projects and expert guidance helped me build confidence in web development.",
+      cover: "./images/testo/t1.webp",
+    },
+    {
+      id: 2,
+      name: "Emma Wilson",
+      post: "WordPress Developer",
+      desc: "The WordPress course was perfect for beginners like me. I learned so much about website building, and the practical examples were very helpful.",
+      cover: "./images/testo/t2.webp",
+    },
+    {
+      id: 3,
+      name: "Michael Brown",
+      post: "FullStack Developer",
+      desc: "The course equipped me with front-end and back-end skills. The projects and curriculum prepared me well for full-stack development.",
+      cover: "./images/testo/t3.webp",
+    },
+  ]
+  
